18b3d5bc60397af72bdcd16235601425967e162e
Unknown <goodmang@etsu.edu>
2018-06-25 11:40:28 -0400

    DBS-03 Deleted Duplicate data then created a unique constraint on CustomerOrderId and ProductId

--- DeveloperInterview.Database/up/DBS-03_AddUniqueConstraintOnOrderProductColumn.sql
+++ DeveloperInterview.Database/up/DBS-03_AddUniqueConstraintOnOrderProductColumn.sql
@@ -1,2 +1,8 @@
+WITH CTE AS(
+  select ProductId, CustomerOrderId, RN = ROW_NUMBER() OVER(PARTITION BY CustomerOrderId, ProductId ORDER BY ProductId)
+  from dbo.OrderProduct
+)
+DELETE FROM CTE WHERE RN > 1
+
 ALTER TABLE dbo.OrderProduct
-ADD UNIQUE (ProductID)
+ADD CONSTRAINT UQ_CustomerOrderId_OrderId UNIQUE(CustomerOrderId, ProductId)
