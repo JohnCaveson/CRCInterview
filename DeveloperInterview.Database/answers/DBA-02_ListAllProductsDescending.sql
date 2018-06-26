179df53072807b76cc591ea3a5457aced5829512
Unknown <goodmang@etsu.edu>
2018-06-26 14:46:27 -0400

    DBA-02 Complete final script for descending order of products

--- DeveloperInterview.Database/answers/DBA-02_ListAllProductsDescending.sql
+++ DeveloperInterview.Database/answers/DBA-02_ListAllProductsDescending.sql
@@ -1,4 +1,6 @@
-SELECT p.Name, op.Quantity
+SELECT p.Id, p.Name, SUM(op.Quantity) AS ItemCount
 FROM dbo.Product p
 JOIN dbo.OrderProduct op
 ON p.Id = op.ProductId
+GROUP BY p.Name, p.Id
+ORDER BY ItemCount DESC
